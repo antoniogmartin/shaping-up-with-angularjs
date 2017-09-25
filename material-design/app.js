f45903e7ea35ad4ae7d4f9cac351569149ce6654
antoniogmartin <antoniogmartin@correo.ugr.es>
2017-09-25 12:58:10 +0200

    material-design

---
+++ material-design/app.js
@@ -0,0 +1,53 @@
+angular.module('colorsDemo', ['ngMaterial'])
+.config(function ($mdThemingProvider, $mdIconProvider) {
+  $mdThemingProvider.theme('forest')
+    .primaryPalette('brown')
+    .accentPalette('green');
+
+  $mdIconProvider
+    .defaultIconSet('material-design-icons-master/sprites/svg-sprite/svg-sprite-social-symbol.svg', 24);
+})
+.directive('regularCard', function () {
+  return {
+    restrict: 'E',
+    templateUrl: 'regularCard.tmpl.html',
+    scope: {
+      name: '@',
+    }
+  }
+})
+.directive('userCard', function () {
+  return {
+    restrict: 'E',
+    templateUrl: 'userCard.tmpl.html',
+    scope: {
+      name: '@',
+      theme: '@'
+    },
+    controller: function ($scope) {
+      $scope.name= $scope.name || 'Antonio';
+      $scope.theme = $scope.theme || 'default';
+    }
+
+
+  }
+
+  
+})
+
+.directive('tableCard', function () {
+    return {
+      restrict: 'E',
+      templateUrl: 'tableCard.tmpl.html',
+      scope: {
+        name: '@'
+      },
+      controller: function ($scope) {
+        $scope.name= $scope.name || 'Desconocido';
+      }
+  
+  
+    }
+})
+
+;
